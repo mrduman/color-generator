08e0711e930d4166d714b33831d8647aca5d91f1
mrduman <omer.duman@gmx.de>
2023-02-19 20:09:55 +0100

    Color Generated created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Values from "values.js";
+import SingleColor from "./SingleColor";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [list, setList] = useState(new Values("#f15025").all(10));
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="container">
+        <form onSubmit={handleSubmit} style={{ display: "flex" }}>
+          <h3>Color Generator</h3>
+          <input
+            className={error ? "error" : ""}
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>{" "}
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              {...color}
+              hexColor={color.hex}
+              key={index}
+              index={index}
+            />
+          );
+        })}
+      </section>
+    </>
   );
 }
 
---
+++ src/SingleColor.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+function SingleColor({ rgb, weight, hexColor, index }) {
+  const [alert, setAlert] = useState(false);
+  const [buttonText, setButtonText] = useState("Copy");
+  const bcg = rgb.join(",");
+  const hexColorOnBoard = `#${hexColor}`;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
+      {console.log("INDEX", index)}
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexColorOnBoard}</p>
+      <button
+        onClick={() => {
+          setAlert(true);
+          // setButtonText(`${alert ? "Copied" : "Copy"}`);
+          setButtonText("Copied");
+          navigator.clipboard.writeText(hexColorOnBoard);
+        }}
+        type="button"
+        className="copyBtn"
+        style={{ padding: ".2rem .4rem", borderRadius: "5px" }}
+      >
+        {buttonText}
+      </button>
+    </article>
+  );
+}
+
+export default SingleColor;
